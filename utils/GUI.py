02c96329d08968933eda303dff406a5017c878bb
Joshua McGrath-Rodriguez <jrod4601@gmail.com>
2025-12-07 11:48:52 -0500

    Added more windows and began the function "add_new_lang" to add a new language

--- utils/GUI.py
+++ utils/GUI.py
@@ -10,6 +10,14 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        tabs = QTabWidget()
+        tabs.addTab(DictionaryTab(), "Dictionary")
+        tabs.addTab(PhonologyTab(), "Phonology")
+
+
+class TemplateTab(QWidget):
+    def __init__(self):
+        super().__init__()
 
 
 class DictionarySelectorDialog(QDialog):
@@ -17,11 +25,56 @@ class DictionarySelectorDialog(QDialog):
         super().__init__()
         self.setWindowTitle("Picking")
         self.resize(500, 500)
+
+        add_new = QPushButton("Add new language")
+        add_new.clicked.connect(self.add_new)
         combo_box = QComboBox()
+        combo_box_label = QLabel("Pick a language")
 
         layout = QGridLayout()
-        layout.addWidget(combo_box)
+        layout.addWidget(add_new)
+        if combo_box:
+            layout.addWidget(combo_box)
         self.setLayout(layout)
+
+    def add_new(self):
+        window = CreateNewLanguage()
+        window.exec()
 
     def get_selected_db(self):
         pass
+
+class PhonologyTab(TemplateTab):
+    def __init__(self):
+        super().__init__()
+
+class DictionaryTab(TemplateTab):
+    def __init__(self):
+        super().__init__()
+
+class PopUpTemplate(QDialog):
+    def __init__(self):
+        super().__init__()
+
+    def add_new_lang(self, db_name: str):
+        db_name = DatabaseManager(db_name)
+
+
+class CreateNewLanguage(PopUpTemplate):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Create New Language")
+        self.resize(300, 300)
+
+        welcome_label = QLabel("Welcome to my Conlang App!\nLet's get started with some basic info.")
+        name_label = QLabel("Name")
+        name_input = QLineEdit()
+
+        enter_button = QPushButton("Done")
+
+
+        widgets = [welcome_label, name_label]
+
+        layout = QGridLayout()
+
+
