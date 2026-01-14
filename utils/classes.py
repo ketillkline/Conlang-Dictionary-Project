ebc906ad2d4d7802d16b317315f564f2f5058af8
Joshua McGrath-Rodriguez <jrod4601@gmail.com>
2026-01-13 19:45:11 -0500

    Reformed. Figuring out phonology and vowel frequency

--- utils/classes.py
+++
@@ -1,3 +0,0 @@
-class Conlang:
-    def __init__(self):
-        pass
--- utils/main.py
+++ utils/main.py
@@ -1,14 +1,36 @@
-from PyQt6.QtWidgets import QApplication, QDialog
-from GUI import MainWindow, DictionarySelectorDialog
-import sys
+import csv, os
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    selector = DictionarySelectorDialog()
-    if selector.exec() == QDialog.DialogCode.Accepted:
-        db_name = selector.get_selected_db()
-        window = MainWindow()
-        window.show()
-        sys.exit(app.exec())
-    else:
-        sys.exit()
+path = r"./Files/Arkabanga.csv"
+words = {
+
+}
+with open(path, encoding="UTF8") as file:
+    csv_reader = csv.reader(file)
+
+    for row in csv_reader:
+        cat, word, meaning, shelf1, shelf2, shelf3 = row
+        words[word] = meaning
+
+# ------- Get close to 0(1) time ------------------ #
+
+target_letter = "o"
+word_list = []
+vowels = ['a', 'i', 'o', 'e', 'u', 'o']
+vowel_count = {}
+for key in words.keys():
+    word_list.append(key)
+for word in word_list:
+    for letter in word:
+        for vowel in vowels:
+            if letter == vowel:
+                if vowel in vowel_count.keys():
+                    vowel_count[vowel]+=1
+                else:
+                    vowel_count[vowel] = 1
+
+for key, value in vowel_count.items():
+    print(f"{key}: {value}")
+
+# -------------------------------------------------- #
+# ----- What consonants do we have? ----------- #
+
